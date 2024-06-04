b767cb2c2e15a4d60fb394405aa4de519049d80d
Cody Perry <perrysdevelopment@gmail.com>
2024-06-03 20:04:08 -0400

    Add text and button styles

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,30 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello there!</h1>
+    <main className='m-4'>
+      {/* Base Card */}
+      <section className='bg-white rounded px-3 py-12'>
+        {/* Heading */}
+        <h1 className='text-4xl text-neutral-900 font-semibold mb-4'>
+          Well crafted abstract images
+        </h1>
+        {/* Description */}
+        <p className='text-lg font-normal text-neutral-600 mb-8'>
+          High quality abstract images for your projects, wallpaper and
+          presentations.
+        </p>
+        {/* Buttons */}
+        <div className='flex gap-4'>
+          <button className='text-base text-neutral-900 font-medium px-5 py-3 border-[0.5px] bg-white rounded grow shadow'>
+            Learn more
+          </button>
+          <button className='text-base text-white font-medium bg-indigo-700 px-5 py-3 rounded grow shadow'>
+            See pricing
+          </button>
+        </div>
+        {/* Image */}
+        {/* <Image src='/prism.png' alt='Prism Hero' fill /> */}
+      </section>
     </main>
   );
 }
