173a95613f8c0b3d964cd53c132786d3408c7503
Cody Perry <perrysdevelopment@gmail.com>
2024-06-03 21:47:31 -0400

    Add pseudo class states for buttons

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className='m-4'>
+    <main className='m-4' role='main'>
       {/* Base Card */}
       <section className='bg-gradient-to-b from-gray-50 to-[#EDF0F3] py-14 lg:py-24 shadow-sm md:shadow-md lg:shadow-lg rounded'>
         <div className='rounded px-3 py-12 lg:flex lg:px-24 lg:py-0 lg:gap-8'>
@@ -19,10 +19,10 @@ export default function Home() {
             </p>
             {/* Buttons */}
             <div className='flex gap-4 md:gap-8 mb-12 md:mb-8'>
-              <button className='text-base md:text-lg text-neutral-900 font-medium px-5 py-3 md:px-12 md:py-4 border-[0.5px] bg-white rounded grow md:grow-0 shadow'>
+              <button className='text-base md:text-lg text-neutral-900 font-medium px-5 py-3 md:px-12 md:py-4 border-[0.5px] bg-white rounded grow md:grow-0 shadow hover:bg-[#FAFAFA] focus:outline-none focus:ring-4 focus:ring-offset-7 focus:ring-indigo-100 focus:rounded-sm focus:bg-[#FAFAFA] disabled:cursor-not-allowed disabled:bg-[#F5F5F5] disabled:text-[#A3A3A3]'>
                 Learn more
               </button>
-              <button className='text-base md:text-lg text-white font-medium bg-indigo-700 px-5 py-3 md:px-12 md:py-4 rounded grow md:grow-0 shadow'>
+              <button className='text-base md:text-lg text-white font-medium bg-indigo-700 px-5 py-3 md:px-12 md:py-4 rounded grow md:grow-0 shadow hover:bg-[#3730A3] focus:outline-none focus:ring-4 focus:ring-offset-7 focus:ring-indigo-100 focus:rounded-sm disabled:cursor-not-allowed disabled:bg-[#F5F5F5] disabled:text-[#A3A3A3]'>
                 See pricing
               </button>
             </div>
@@ -32,7 +32,7 @@ export default function Home() {
             <Image
               className='rounded-2xl'
               src='/prism.png'
-              alt='Prism Hero'
+              alt='Abstract prism hero image'
               width={640}
               height={484}
               layout='responsive'
